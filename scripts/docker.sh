f7f60920b05b5133e0aa570c23d646827144d3b1
EOussama <oussama_gtacj@live.fr>
2023-06-16 12:32:38 +0100

    💢 Angular Init

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,2 +1,2 @@
 docker build -f Dockerfile.dev -t eoussama/truedrive_app ..
-docker run -it --rm -v "$(pwd)":/app eoussama/truedrive_app /bin/sh
+docker run -it --rm -v "$(pwd)":/app -p 4200:4200 eoussama/truedrive_app /bin/sh
